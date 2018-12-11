16ea7dc0c851e8f8eb9e4590c86f4060a707085a
Vernou Cédric <Orwel@users.noreply.github.com>
2018-12-12 00:45:43 +0100

    #12 Add IssueViewModel to Issue page (PR #13)

---
+++ src/Chronos.Core.Test/IssueViewModelTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Xunit;
+
+namespace Chronos.Core.Test
+{
+    public class IssueViewModelTest
+    {
+        [Fact]
+        public void KeyEqualLabel()
+        {
+            var vm = new IssueViewModel(new ChronometerFake());
+            vm.Key = "A key";
+            Assert.Equal("A key", vm.Label);
+        }
+
+        [Fact]
+        public void RunningWhenChronometerIsRunning()
+        {
+            var vm = new IssueViewModel(new ChronometerFake { Running = true });
+            Assert.True(vm.Running);
+        }
+
+        [Fact]
+        public void NotRunningWhenChronometerIsNotRunning()
+        {
+            var vm = new IssueViewModel(new ChronometerFake { Running = false });
+            Assert.False(vm.Running);
+        }
+
+        [Fact]
+        public void ElapsedFromChronometer()
+        {
+            var vm = new IssueViewModel(new ChronometerFake { Elapsed = new TimeSpan(1, 2, 3) });
+            Assert.Equal(new TimeSpan(1, 2, 3), vm.Elapsed);
+        }
+
+        [Fact]
+        public void ElapsedLabelFromChronometer()
+        {
+            var vm = new IssueViewModel(new ChronometerFake { Elapsed = new TimeSpan(1, 2, 3) });
+            Assert.Equal("01h02m03s", vm.ElapsedLabel);
+        }
+
+        [Fact]
+        public void StartStopButtonOnClickWhenNotRunning()
+        {
+            var vm = new IssueViewModel(new ChronometerFake { Running = false });
+            vm.StartStopButtonOnClick();
+            Assert.True(vm.Running);
+        }
+
+        [Fact]
+        public void StartStopButtonOnClickWhenRunning()
+        {
+            var vm = new IssueViewModel(new ChronometerFake { Running = true });
+            vm.StartStopButtonOnClick();
+            Assert.False(vm.Running);
+        }
+    }
+}
---
+++ src/Chronos.Core/IssueViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Chronos.Core
+{
+    public sealed class IssueViewModel
+    {
+        private readonly Chronometer _chronometer;
+
+        public IssueViewModel(Chronometer chronometer)
+        {
+            _chronometer = chronometer;
+        }
+
+        public string Key { get; set; }
+        public string Label => Key;
+        public bool Running => _chronometer.Running;
+        public TimeSpan Elapsed => _chronometer.Elapsed;
+        public string ElapsedLabel => $"{Elapsed.Hours:00}h{Elapsed.Minutes:00}m{Elapsed.Seconds:00}s";
+
+        public void StartStopButtonOnClick() => _chronometer.Running = !_chronometer.Running;
+    }
+}
